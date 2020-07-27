a3eb18af492ff83aa1187d8fd2c5f63be7c7d34d
Nico van der Linden <nico@Noki-Server.local>
2020-07-27 18:23:36 +0200

    enable XCODE

---
+++ fastLaneTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fastLaneTest
+//
+//  Created by Nico van der Linden on 27/07/2020.
+//  Copyright © 2020 Nico van der Linden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
